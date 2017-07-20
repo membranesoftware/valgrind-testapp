150d65d841b422dacca4464b6c0f223c97334a77
Membrane <author@membranesoftware.com>
2017-07-20 10:59:44 -0700

    testapp now causes a memory leak

--- testapp.c
+++ testapp.c
@@ -1,5 +1,13 @@
 #include <stdlib.h>
 
 int main (int argc, char **argv) {
+	int len, *array;
+
+	len = 8;
+	array = (int *) malloc (len * sizeof (int));
+
+	// TODO: Free this array (currently leaving a leak for detection by valgrind)
+	// free (array);
+
 	exit (0);
 }
